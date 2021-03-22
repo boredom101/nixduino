1422448e715ffc15fc32c20c495dd3d0827bb10a
boredom101 <ymnewmark@gmail.com>
2021-03-22 22:48:51 +0200

    Update arduino.nix

--- arduino.nix
+++ arduino.nix
@@ -6,11 +6,11 @@ let
   extraArgs = removeAttrs args [ "name" "board" "libraries" ];
 in stdenv.mkDerivation({
   buildInputs = [ arduino-mk ];
-  makefile = writeScript "makefile" ''
+  makefile = (writeScript "makefile" ''
     BOARD_TAG = ${board}
     ARDUINO_LIBS = ${lib.concatStringsSep " " libraries}
     include ${arduino-mk}/Arduino.mk
-  ''.outPath;
+  '').outPath;
   installPhase = ''
     mkdir -p $out
     mv build-${board}/$(basename $PWD).hex $out/build.hex
