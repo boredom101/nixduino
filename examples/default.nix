dc5639ef56bf04a6e22f6a06d6401f65741ad2ef
boredom101 <ymnewmark@gmail.com>
2021-03-22 22:46:11 +0200

    Update default.nix

--- examples/default.nix
+++ examples/default.nix
@@ -1,6 +1,6 @@
 { board, pkgs ? import <nixpkgs> {} }:
 
-import ../arduino.nix { inherit (pkgs) stdenv lib arduino-mk; } {
+import ../arduino.nix { inherit (pkgs) stdenv lib arduino-mk writeScript; } {
   name = "blink";
   board = board;
   libraries = [];
