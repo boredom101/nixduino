061f493a1a5a35537f0e4174607457c122cba0ca
Daniel Nagy <danielnagy@posteo.de>
2022-05-15 20:50:56 +0200

    Demonstrate usage of callPackage in examples
    This allows users of the package to not needing to know what arguments
    the derivation is expecting.

--- examples/blink/default.nix
+++ examples/blink/default.nix
@@ -1,6 +1,6 @@
 { board, pkgs ? import <nixpkgs> {} }:
 
-import ../../arduino.nix { inherit (pkgs) stdenv lib arduino-mk writeScript arduino-core-unwrapped; } {
+pkgs.callPackage ../../arduino.nix { } {
   name = "blink";
   board = board;
   libraries = [];
